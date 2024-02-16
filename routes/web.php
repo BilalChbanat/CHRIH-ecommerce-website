a20857b253891f85380d7580c52eba5319e8aa50
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-02-16 09:22:37 +0100

    setup my branch

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// Comment out or remove the Voyager routes
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
