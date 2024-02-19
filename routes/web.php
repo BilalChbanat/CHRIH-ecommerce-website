aec512d578d316fbc4633e2895fdf9a2f59c186b
MedELHattab <elhattabmohammedelarbi.com>
2024-02-19 10:40:31 +0100

    I added the card functionality and the views for products, cart and shop

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,9 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Route::get('/home', [ProductController::class, 'index']);  
+Route::get('/shopping-cart', [ProductController::class, 'productCart'])->name('shopping.cart');
+Route::get('/product/{id}', [ProductController::class, 'addProducttoCart'])->name('addproduct.to.cart');
+Route::patch('/update-shopping-cart', [ProductController::class, 'updateCart'])->name('update.shopping.cart');
+Route::delete('/delete-cart-product', [ProductController::class, 'deleteProduct'])->name('delete.cart.product');
